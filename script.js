205dd685d68f9442bc89d3a4c8fd4e1fbdc0fa7f
fabih1996 <153489621+fabih1996@users.noreply.github.com>
2024-06-30 17:05:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 document.getElementById('play-button').addEventListener('click', function() {
     var videoContainer = document.getElementById('video-container');
-    var video = document.getElementById('video');
     videoContainer.style.display = 'flex';
-    video.play();
 });
 
 document.getElementById('close-button').addEventListener('click', function() {
     var videoContainer = document.getElementById('video-container');
     var video = document.getElementById('video');
     video.pause();
+    video.currentTime = 0; // Riporta il video all'inizio quando si chiude
     videoContainer.style.display = 'none';
 });
 
